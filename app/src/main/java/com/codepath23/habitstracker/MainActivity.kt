2434dfcbad8be3d31190b559324e0f52e7a3accf
jatruong <jatruong@uw.edu>
2022-04-09 11:44:00 -0700

    Initial clean commit

---
+++ app/src/main/java/com/codepath23/habitstracker/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.codepath23.habitstracker
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
