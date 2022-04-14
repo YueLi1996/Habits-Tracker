1d66c40a212a63a2d2fe125581f0d5f824018464
jatruong <jatruong@uw.edu>
2022-04-14 13:42:12 -0700

    Updated to include parse API and library.

--- app/src/main/java/com/codepath23/habitstracker/model/Habit.kt
+++ app/src/main/java/com/codepath23/habitstracker/model/Habit.kt
@@ -1,5 +1,8 @@
 package com.codepath23.habitstracker.model
 
+import com.parse.ParseClassName
+
+@ParseClassName("Habit")
 class Habit {
     //TODO
 }
--- app/src/main/java/com/codepath23/habitstracker/model/User.kt
+++ app/src/main/java/com/codepath23/habitstracker/model/User.kt
@@ -1,5 +1,8 @@
 package com.codepath23.habitstracker.model
 
+import com.parse.ParseClassName
+
+@ParseClassName("User")
 class User {
     //TODO
 }
